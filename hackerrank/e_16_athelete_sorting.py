bdac97c2024653292e659ba420bb908056b84826
Musfiqur Rahman <md.musfiqurcse@outlook.com>
2020-04-19 21:18:53 +0200

    Use the sorted the dynamic array by enhancing the key function of sorted  builtin function.

---
+++ hackerrank/e_16_athelete_sorting.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+counter = 0
+
+def checker_item(elem):
+    return elem[counter]
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    counter = k
+    list_item = sorted(arr,key=checker_item)
+    for i in range(0, n):
+        res =""
+        for k in range(0,m):
+            res+="{0}".format(list_item[i][k])
+            if k!=m-1:
+                res+=" "
+        print(res)
