ef90a7c19d1e5c20a64927dc764737aefb6c942e
Musfiqur Rahman <md.musfiqurcse@outlook.com>
2020-04-21 13:27:16 +0200

    Ignore the exception case and then consider the leap year logic.

---
+++ hackerrank/e_17_day_of_programmer.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year):
+    if year == 1918:
+        return "26.09.1918"
+    if year < 1918:
+        return "12.09.{0}".format(year) if year%4==0 else "13.09.{0}".format(year)
+    return "12.09.{0}".format(year) if year%400 == 0 or (year%4==0 and year%100!=0) else "13.09.{0}".format(year)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
