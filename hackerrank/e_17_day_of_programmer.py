4e842fbf69f5e476e2f104817108b2cf1902f9ac
Musfiqur Rahman <md.musfiqurcse@outlook.com>
2020-04-23 23:53:30 +0200

    [Update] Georgian Calendar Condition added

--- hackerrank/e_17_day_of_programmer.py
+++ hackerrank/e_17_day_of_programmer.py
@@ -10,6 +10,7 @@ import sys
 def dayOfProgrammer(year):
     if year == 1918:
         return "26.09.1918"
+    # Condition added for Georgian Calendar
     if year < 1918:
         return "12.09.{0}".format(year) if year%4==0 else "13.09.{0}".format(year)
     return "12.09.{0}".format(year) if year%400 == 0 or (year%4==0 and year%100!=0) else "13.09.{0}".format(year)
