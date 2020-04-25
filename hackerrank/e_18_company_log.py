400aae2bb8493b3900006cf0e4c851b07a6459ed
Musfiqur Rahman <md.musfiqurcse@outlook.com>
2020-04-25 12:10:56 +0200

    [Sorted][Dict]Need to use sorted function for ASC -1 in index and for DESC nothing

---
+++ hackerrank/e_18_company_log.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def input_string(st):
+    main_dicti = {}
+    jj = []
+    for i in st:
+        if i not in main_dicti:
+            main_dicti[i] = 1
+        else:
+            main_dicti[i]+=1 
+    jj = sorted(main_dicti.items(), key=lambda x:(-x[1],x[0]))
+    for k in range(0,3):
+        print(jj[k][0],jj[k][1])
+        
+
+
+if __name__ == '__main__':
+    s = input()
+    input_string(s)
